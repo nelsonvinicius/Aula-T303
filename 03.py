6fe24f15ac52b4036ff3ce0565f33d5144067db8
nelsonvinicius <nel.viniciuus@gmail.com>
2019-07-19 17:14:09 -0300

    subir

---
+++ 03.py
@@ -0,0 +1,25 @@
+
+# lista('Mica, Bana)
+
+# def lista_de_compras(Mica, item1, item2, item3, item4):
+#     print(f'Lista de compras de {pessoa}:')
+#     print(item1)
+#     print(item2)
+#     print(item3)
+#     print(item4)
+
+# def lista_de_compras(**kwargs):
+#     frut = kwargs.get('fruta')
+#     if fruta is not Nome:
+#         print(f'Na lista de compras há uma fruta: {fruta}')
+
+# lista_de_compras(fruta= 'banana', massas='nhoque', verdura='alface')        
+# lista_de_compras(bebida='guarana', srovete='flocos')
+
+class Animal:
+    def __init__(self, nome, dono):
+    self.nome = Nome
+    self.dono = dono
+
+    def latir(self):
+        print('Auauau Auaua', self.nome)
